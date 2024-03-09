fcb2b9530351a845fb397222147b7c4c2465402a
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-03-09 17:19:20 +1100

    Update endpoint links and remove meme endpoint (reddit api sucks)

--- public/js/endpoints.js
+++ public/js/endpoints.js
@@ -1,67 +1,62 @@
 const endpoints = [
 {
 	"endpoint": "QR Code Generator",
-	"link": "https://api.dhyan99.is-a.dev/qr?text=Hello&download=false",
+	"link": "https://d99api.vercel.app/qr?text=Hello&download=false",
 	"description": "Imagine typing, just scan a QR code instead"
 },
 {
 	"endpoint": "Image Inverter",
-	"link": "https://api.dhyan99.is-a.dev/invert?url=https://media.timeout.com/images/103161245/image.jpg",
+	"link": "https://d99api.vercel.app/invert?url=https://media.timeout.com/images/103161245/image.jpg",
 	"description": "Images were never meant to be this way"
 },
 {
 	"endpoint": "Image Blur",
-	"link":"https://api.dhyan99.is-a.dev/blur?url=https://media.timeout.com/images/103161245/image.jpg&strength=7",
+	"link":"https://d99api.vercel.app/blur?url=https://media.timeout.com/images/103161245/image.jpg&strength=7",
 	"description":"No one will ever know what was in the image"
 },
 {
 	"endpoint": "Image Greyscale",
-	"link":"https://api.dhyan99.is-a.dev/greyscale?url=https://media.timeout.com/images/103161245/image.jpg",
+	"link":"https://d99api.vercel.app/greyscale?url=https://media.timeout.com/images/103161245/image.jpg",
 	"description":"Welcome to the dark side"
 },
 {
 	"endpoint": "Weather",
-	"link": "https://api.dhyan99.is-a.dev/weather?location=sydney",
+	"link": "https://d99api.vercel.app/weather?location=sydney",
 	"description": "Now you'll know \"weather\" or not you need a jacket"
 },
 {
-	"endpoint": "Memes",
-	"link": "https://api.dhyan99.is-a.dev/meme",
-	"description": "You can never have enough memes"
-},
-{
 	"endpoint": "Currency Value",
-	"link": "https://api.dhyan99.is-a.dev/value?currency=AUD",
+	"link": "https://d99api.vercel.app/value?currency=AUD",
 	"description": "Wait, $1 isn't always worth $1"
 },
 {
 	"endpoint": "Lyrics",
-	"link": "https://api.dhyan99.is-a.dev/lyrics?song=YourFavouriteSong",
+	"link": "https://d99api.vercel.app/lyrics?song=YourFavouriteSong",
 	"description": "Now you can read your favourite song"
 },
 {
 	"endpoint": "Quotes",
-	"link": "https://api.dhyan99.is-a.dev/quote",
+	"link": "https://d99api.vercel.app/quote",
 	"description": "Nothin' better than a quote to start your day"
 },
 {
 	"endpoint": "Crypto Value",
-	"link": "https://api.dhyan99.is-a.dev/crypto?currency=DOGE",
+	"link": "https://d99api.vercel.app/crypto?currency=DOGE",
 	"description": "I must buy more Doge"
 },
 {
 	"endpoint": "NPM Package Info",
-	"link": "https://api.dhyan99.is-a.dev/npm?package=xtra-express",
+	"link": "https://d99api.vercel.app/npm?package=xtra-express",
 	"description": "Know more about that really useful package"
 },
 {
 	"endpoint": "Morse-ify",
-	"link":"https://api.dhyan99.is-a.dev/morseify?text=English-is-Inferior",
+	"link":"https://d99api.vercel.app/morseify?text=English-is-Inferior",
 	"description":"Now you won't know what you wrote either"
 },
 {
 	"endpoint":"UnMorse-ify",
-	"link":"https://api.dhyan99.is-a.dev/unmorseify?morse=.%20-.%20--.%20.-..%20..%20...%20....%20-....-%20..%20...%20-....-%20...%20..-%20.--.%20.%20.-.%20..%20---%20.-.",
+	"link":"https://d99api.vercel.app/unmorseify?morse=.%20-.%20--.%20.-..%20..%20...%20....%20-....-%20..%20...%20-....-%20...%20..-%20.--.%20.%20.-.%20..%20---%20.-.",
 	"description":"Now you can finally understand those dots and dashes "
 }
 ]
